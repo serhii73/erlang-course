36472d09fc6a6fc13de2586093afb892cc95acea
Serhii A <aserhii@protonmail.com>
2020-11-11 19:01:25 +0200

    Add bs01

--- tasks_3/bs01.erl
+++ tasks_3/bs01.erl
@@ -2,5 +2,17 @@
 
 -export([first_word/1]).
 
-first_word(Bin) ->
-   lists:nth(1, binary:split(Bin,<<" ">>)).
+% first_word(Bin) ->
+%    lists:nth(1, binary:split(Bin,<<" ">>)).
+
+first_word(B) ->
+   first_word(B, <<>>).
+
+first_word(<< " ",   Rest/binary>>, <<>>) ->
+   first_word(Rest, <<>>);
+first_word(<<" ", _/binary>>, Acc) ->
+   Acc;
+first_word(<<C/utf8, Rest/binary>>, Acc) ->
+   first_word(Rest, <<Acc/binary, C/utf8>>);
+first_word(<<>>, Acc) ->
+   Acc.
